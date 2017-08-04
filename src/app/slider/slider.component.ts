fa679d3cc538af9fecac8ee78486f9bd3a5f3e3b
Aditya Bhandari <adityab@vmware.com>
2017-08-04 16:09:38 -0400

    Day 1
    Signed-off-by: Aditya Bhandari <adityab@vmware.com>

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Input, ViewChild} from '@angular/core';
+import {DragDispatcher} from "clarity-angular/data/datagrid/providers/drag-dispatcher"
+import {Subscription} from "rxjs/Subscription";
 
 @Component({
     selector: 'app-slider',
     templateUrl: './slider.component.html',
-    styleUrls: ['./slider.component.scss']
+    styleUrls: ['./slider.component.scss'],
+    providers: [DragDispatcher]
 })
-export class SliderComponent implements OnInit {
+export class SliderComponent implements AfterViewInit {
 
-    constructor() { }
+    private subscriptions: Subscription[] = [];
+    private _translateX: number = 0;
+    private _sliderWidth: number = 0;
+    private _slider: any;
+    private _value: number = 0;
 
-    ngOnInit() {
+    constructor(private dragDispatcher: DragDispatcher) {
+        this.subscriptions.push(
+            this.dragDispatcher.onDragMove.subscribe(($event) => {
+                this.onMove($event.clientX);
+            }));
     }
 
+    get value(): number {
+        return this._value;
+    }
+
+    @Input("clrValue")
+    set value(val: number) {
+        this._value = val;
+    }
+
+    @ViewChild("thumb")
+    set thumb(value: ElementRef) {
+        this.dragDispatcher.handleRef = value;
+        this.dragDispatcher.addDragListener();
+    }
+
+    @ViewChild("slider")
+    set slider(value: ElementRef) {
+        this._slider = value;
+    }
+
+    get translateX(): number {
+        return this._translateX;
+    }
+
+    ngAfterViewInit() {
+        this._sliderWidth = this._slider.nativeElement.getBoundingClientRect().width;
+    }
+
+    get translateValue(): string {
+        return "translate3d(" + this.value + "px" + ", -50%, 0)";
+    }
+
+    onMove(value: number): void {
+        this.value = value;
+    }
 }
